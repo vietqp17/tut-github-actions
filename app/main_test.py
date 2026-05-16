75184784f6692bce15ecc0336d1696c7a4c6e17b
vietqp17 <vietqp@uia.no>
2026-05-16 14:19:51 +0200

    first commit

---
+++ app/main_test.py
@@ -0,0 +1,19 @@
+from main import return_backward_string, get_mode
+import unittest
+import os
+
+
+class TestMain(unittest.TestCase):
+
+    def test_return_backward_string(self):
+        random_string = 'This is my test string'
+        random_string_reversed = 'gnirts tset ym si sihT'
+        self.assertEqual(random_string_reversed,
+                         return_backward_string(random_string))
+
+    def test_get_env(self):
+        self.assertEqual(os.environ.get("MODE"), get_mode())
+
+
+if __name__ == '__main__':
+    unittest.main()
